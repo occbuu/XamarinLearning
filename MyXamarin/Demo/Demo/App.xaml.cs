8e4c7df2d8cf6ea89674b05d5578b84f964e1b24
zngtfy <nvt87x@gmail.com>
2017-11-02 14:22:39 +0700

    Sync code

--- MyXamarin/Demo/Demo/App.xaml.cs
+++ MyXamarin/Demo/Demo/App.xaml.cs
@@ -2,28 +2,50 @@
 
 namespace Demo
 {
+    /// <summary>
+    /// App
+    /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
+        #region -- Overrides --
 
-            MainPage = new Views.MainMenu();
-        }
-
+        /// <summary>
+        /// On start
+        /// </summary>
         protected override void OnStart()
         {
             // Handle when your app starts
         }
 
+        /// <summary>
+        /// On sleep
+        /// </summary>
         protected override void OnSleep()
         {
             // Handle when your app sleeps
         }
 
+        /// <summary>
+        /// On resume
+        /// </summary>
         protected override void OnResume()
         {
             // Handle when your app resumes
         }
+
+        #endregion
+
+        #region -- Methods --
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new Menu();
+        }
+
+        #endregion
     }
 }
