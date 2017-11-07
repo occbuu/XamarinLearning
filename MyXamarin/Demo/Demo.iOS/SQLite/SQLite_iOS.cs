0e23c022fff533c2a21d4a76bb39e91908915fb4
levandiep44@gmail.com <levandiep44@gmail.com>
2017-11-07 10:54:26 +0700

    Update SQLite for IOS

---
+++ MyXamarin/Demo/Demo.iOS/SQLite/SQLite_iOS.cs
@@ -0,0 +1,33 @@
+﻿
+using Demo.iOS.SQLite;
+using SQLite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+
+namespace Demo.iOS.SQLite
+{
+    using Demo.Services;
+
+    public class SQLite_iOS : ISQLite
+    {
+
+        public SQLite_iOS()
+        {
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "MySQLiteDB.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
